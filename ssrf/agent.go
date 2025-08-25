250eb39da86df396ca8ec8ae352d075c0d44a650
n1k1x86 <i@n1k1x86.ru>
2025-08-25 13:12:39 +1000

    first version - added mvp ssrf agent

---
+++ ssrf/agent.go
@@ -0,0 +1,79 @@
+package ssrf
+
+import (
+	"context"
+	"log"
+
+	rasp_rpc "github.com/n1k1x86/rasp-grpc-contract/gen/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type SSRFClient struct {
+	Stub    rasp_rpc.RASPCentralClient
+	agentID string
+	ctx     context.Context
+}
+
+func (s *SSRFClient) RegAgent(hostRules, ipRules, regexpRules []string,
+	serviceName, serviceDescription, agentName, updateURL string) error {
+	req := NewRegAgentRequest(hostRules, ipRules, regexpRules,
+		serviceName, serviceDescription, agentName, updateURL)
+	resp, err := s.Stub.RegSSRFAgent(s.ctx, req)
+	if err != nil {
+		return err
+	}
+	s.agentID = resp.AgentID
+
+	log.Println(resp.Detail)
+	return nil
+}
+
+func (s *SSRFClient) DeactivateAgent(serviceName, agentName, agentID string) error {
+	req := NewDeactivateAgentRequest(serviceName, agentName, agentID)
+	resp, err := s.Stub.DeactivateSSRFAgent(s.ctx, req)
+	if err != nil {
+		return err
+	}
+
+	log.Println(resp.Detail)
+	return nil
+}
+
+func CloseClient(client *grpc.ClientConn) error {
+	err := client.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewClient(ctx context.Context) (*SSRFClient, error) {
+	client, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		r := recover()
+		if r != nil {
+			log.Printf("panic recovered: %s", r)
+		}
+	}()
+
+	go func() {
+		err := CloseClient(client)
+		if err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	stub := rasp_rpc.NewRASPCentralClient(client)
+
+	ssrfClient := &SSRFClient{
+		Stub: stub,
+		ctx:  ctx,
+	}
+
+	return ssrfClient, nil
+}
---
+++ ssrf/models.go
@@ -0,0 +1,26 @@
+package ssrf
+
+import (
+	rasp_rpc "github.com/n1k1x86/rasp-grpc-contract/gen/proto"
+)
+
+func NewRegAgentRequest(hostRules, ipRules, regexpRules []string,
+	serviceName, serviceDescription, agentName, updateURL string) *rasp_rpc.RegSSRFAgentRequest {
+	return &rasp_rpc.RegSSRFAgentRequest{
+		ServiceName:        serviceName,
+		ServiceDescription: serviceDescription,
+		AgentName:          agentName,
+		UpdateURL:          updateURL,
+		HostRules:          hostRules,
+		IPRules:            ipRules,
+		RegexpRules:        regexpRules,
+	}
+}
+
+func NewDeactivateAgentRequest(serviceName, agentName, agentID string) *rasp_rpc.DeactivateSSRFAgentRequest {
+	return &rasp_rpc.DeactivateSSRFAgentRequest{
+		ServiceName: serviceName,
+		AgentName:   agentName,
+		AgentID:     agentID,
+	}
+}
